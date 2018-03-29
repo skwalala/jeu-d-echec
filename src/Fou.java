098135aad567a96dd53a58a9300934988d5f85e6
Unknown <theo.collignon2506@gmail.com>
2018-03-29 14:11:39 +0200

    Modif Fou
    modification de seDeplace et checkDeplace

--- src/Fou.java
+++ src/Fou.java
@@ -1,26 +1,44 @@
 import java.lang.Math;
 class Fou extends Piece{
 	
+	/*
+	 * constructeur vide
+	 */
 	public Fou(){}
 
-	public Fou(int x, int y, String couleur){
+	/*
+	 * constructeur avec les positions en x, y et la couleur
+	 */
+	public Fou(int x, int y, String couleur){ 
 		super(x,y,couleur,"F");
 	}
 
+	/*
+	 * reçoit une demande de déplacement et l'éxecute si celle-ci est possible
+	 */
 	public void seDeplace(int x, int y, Piece[][] p) {
-		if (checkDeplace(x,y,p)=="true"){
-			Piece temp;
-			temp=p[super.getPosX()][super.getPosY()];
-			p[super.getPosX()][super.getPosY()]=null;
-			this.x=x;
-			this.y=y;
-			p[x][y]=temp;
-		} else if (checkDeplace(x,y,p)=="mange"){
-			this.mange(x,y,p);
+		if (checkDeplace(x,y,p)){
+			if (!(p[x][y]==null)) {
+				if (!(p[x][y].getCouleur()==super.getCouleur())) {
+					mange(x,y,p);
+				}
+			} else {
+				Piece temp;
+				temp=p[super.getPosX()][super.getPosY()];
+				p[super.getPosX()][super.getPosY()]=null;
+				super.setPosX(x);
+				super.setPosY(y);
+				p[x][y]=temp;
+			}
+		} else if (checkDeplace(x,y,p)==false){
+			
 		}
 	}
 
-	public String checkDeplace(int newX, int newY, Piece[][] p){
+	/*
+	 * vérifie si le déplacement est possible
+	 */
+	public boolean checkDeplace(int newX, int newY, Piece[][] p){
 		int initX=super.getPosX();
 		int initY=super.getPosY();
 		int diffX=newX-initX;
@@ -31,51 +49,44 @@ class Fou extends Piece{
 
 		if (diffX==diffY) {
 			if( newX<initX && newY<initY ) {
-				for (int a=initX-1 ; a>newX ; a--){
+				for (int a=initX-1 ; a>newX-1 ; a--){
 					b--;
 					if (!(p[a][b]==null)){
-						if(a==newX && b == newY){
-							return "mange";
-						} else return "false";
+						return false;
 					}
 				}
 			} else if ( newX>initX && newY<initY ) {
-				for (int a=initX+1 ; a<newX ; a++){
+				for (int a=initX+1 ; a<newX-1 ; a++){
 					b--;
 					if (!(p[a][b]==null)){
-						if(a==newX && b == newY){
-							return "mange";
-						} else return "false";
+						return false;
 					}
 				}
 			} else if ( newX<initX && newY>initY ) {
-				for (int a=initX-1 ; a>newX ; a--){
+				for (int a=initX-1 ; a>newX-1 ; a--){
 					b++;
 					if (!(p[a][b]==null)){
-						if(a==newX && b == newY){
-							return "mange";
-						} else return "false";
+						return false;
 					}
 				}
 			} else if ( newX>initX && newY>initY ) {
-				for (int a=initX+1 ; a<newX ; a++){
+				for (int a=initX+1 ; a<newX-1 ; a++){
 					b++;
 					if (!(p[a][b]==null)){
-						if(a==newX && b == newY){
-							return "mange";
-						} else return "false";
+						return false;
 					}
 				}
 			}
 		}
+		return true;
 	}
 
-	public boolean mange(int x, int y, Piece[][] p){
+	public void mange(int x, int y, Piece[][] p){
 		Piece temp;
 		temp=p[super.getPosX()][super.getPosY()];
 		p[super.getPosX()][super.getPosY()]=null;
-		this.x=x;
-		this.y=y;
+		super.setPosX(x);
+		super.setPosY(y);
 		p[x][y]=temp;
 	}
 }
