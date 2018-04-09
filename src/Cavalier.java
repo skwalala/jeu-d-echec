6a2ee2ed8a931ebb54c21aad14b1ceabbdf55acc
alexis <alexiscrinquand@gmail.com>
2018-04-09 17:58:18 +0200

    cavalier.java bien placé
    dans le fichier src

--- src/Cavalier.java
+++ src/Cavalier.java
@@ -22,4 +22,28 @@ public class Cavalier extends Piece{
 		}
 		return false;
 	}
+
+	public int[][] getAllDeplacement(){
+		int[8][8] grille;
+		for(int i=0; i<8; i++){
+			for (int j=0; j<8; j++){
+				if (((super.getPosX()+2==i)&&(super.getPosY()+1==j))||
+					((super.getPosX()+2==i)&&(super.getPosY()-1==j))||
+					((super.getPosX()+1==i)&&(super.getPosY()+2==j))||
+					((super.getPosX()+1==i)&&(super.getPosY()-2==j))||
+					((super.getPosX()-1==i)&&(super.getPosY()+2==j))||
+					((super.getPosX()-1==i)&&(super.getPosY()-2==j))||
+					((super.getPosX()-2==i)&&(super.getPosY()+1==j))||
+					((super.getPosX()-2==i)&&(super.getPosY()-1==j))){
+					if (estOccuppeParPieceEquipe(dx,dy,super.getCouleur())==false){
+						grille[i][j]=1;
+					}
+				}else{
+					grille[i][j]=0;
+				}
+			}
+		}
+		grille[super.getPosX()][super.getPosY()]=1
+		return grille;
+	}
 }
