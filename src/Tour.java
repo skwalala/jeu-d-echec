ae47e81a19f3f6aad1383b7d70343e9fbcf9eb33
Victor <victor.brungard@gmail.com>
2018-04-09 18:56:29 +0200

    maj tour

--- src/Tour.java
+++ src/Tour.java
@@ -20,95 +20,83 @@ public class Tour extends Piece
 
         // methode validiter du mouvement
 
-        public boolean estValide(int deplacementX, int deplacementY, Piece[][] p)
-        {
+        public boolean estValide(int deplacementX, int deplacementY, Piece[][] p) {
 
-          int x =super.getPosX();
-      		int y =super.getPosY();
-          int a = x;
-          int b = y;
-      		int diffX = deplacementX - x;
-      		int diffY = deplacementY - y;
-      		int b=initY;
-      		diffX=Math.abs(diffX);
-      		diffY=Math.abs(diffY);
+            int x = super.getPosX();
+            int y = super.getPosY();
+            int a = x;
+            int b = y;
+            int diffX = deplacementX - x;
+            int diffY = deplacementY - y;
+            diffX = Math.abs(diffX);
+            diffY = Math.abs(diffY);
 
-          if((y * deplacementY == 0) && (x < deplacementX))
-          {
-                for(int i =0; i<diffX; i++)
-              {
-                  if(p[a-1][0]==null){
-                    a--;
-                  }
-                  else{
-                    return false;
-                  }
-              }
+            if ((y * deplacementY == 0) && (x < deplacementX)) {
+                for (int i = 0; i < diffX; i++) {
+                    if (p[a - 1][0] == null) {
+                        a--;
+                    } else {
+                        return false;
+                    }
+                }
 
-          }
+            }
 
-          if((y * deplacementY == 0) && (x > deplacementX))
-          {
-                for(int i =0; i<diffX; i++)
-              {
-                  if(p[a+1][0]==null){
-                    a++;
-                  }
-                  else{
-                    return false;
-                  }
-              }
+            if ((y * deplacementY == 0) && (x > deplacementX)) {
+                for (int i = 0; i < diffX; i++) {
+                    if (p[a + 1][0] == null) {
+                        a++;
+                    } else {
+                        return false;
+                    }
+                }
 
-          }
+            }
 
 
-          if((x * deplacementX == 0) && (y < deplacementY))
-          {
-            for(int i =0; i<diffX; i++)
-          {
-              if(p[0][b-1]==null){
-                b--;
-              }
-              else{
-                return false;
-              }
-          }
+            if ((x * deplacementX == 0) && (y < deplacementY)) {
+                for (int i = 0; i < diffY; i++) {
+                    if (p[0][b - 1] == null) {
+                        b--;
+                    } else {
+                        return false;
+                    }
+                }
+            }
 
-          if((x * deplacementX == 0) && (y > deplacementY))
-          {
-            for(int i =0; i<diffX; i++)
-          {
-              if(p[0][b+1]==null){
-                b++;
-              }
-              else{
-                return false;
-              }
-          }
+            if ((x * deplacementX == 0) && (y > deplacementY)) {
+                for (int i = 0; i < diffY; i++) {
+                    if (p[0][b + 1] == null) {
+                        b++;
+                    } else {
+                        return false;
+                    }
+                }
+            }
 
-
+             return false;
         }
 
-        @Override
+
             public String getNom()
         {
             return super.getNom();
         }
 
-         @Override
+
             public int getPosX()
          {
             return super.getPosX();
          }
 
-         @Override
+
             public int getPosY()
          {
             return super.getPosY();
          }
 
          public boolean seDeplace(int x, int y, Piece[][] p) {
-       		if (checkDeplace(x,y,p)){
+       		if (estValide(x,y,p)){
        			if (!(p[x][y]==null)) {
        				if (!(p[x][y].getCouleur()==super.getCouleur())) {
        					return true;
@@ -116,7 +104,7 @@ public class Tour extends Piece
        			} else {
        				return true;
        			}
-       		} else if (checkDeplace(x,y,p)==false){
+       		} else if (estValide(x,y,p)==false){
        		}
        		return false;
        	}
