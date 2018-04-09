de4e04c5cd4158da69d123eb7871c48acb96ed50
alexis <alexiscrinquand@gmail.com>
2018-04-09 18:11:40 +0200

    maj dame.java

--- src/Dame.java
+++ src/Dame.java
@@ -1,4 +1,4 @@
-import java.util.*
+import java.util.*;
 
 public class Dame extends Piece{
 	public Dame(int x, int y, String couleur){
@@ -11,7 +11,7 @@ public class Dame extends Piece{
 		else{
 			int i;
 			for (i=0; i>=super.getPosX()-dx; i--){} //place i en tant que différence entre la position initiale et la position demandée
-			for (i; i<super.getPosX()-dx; i++){} //pareil mais dans l'autre sens
+			for (i=i; i<super.getPosX()-dx; i++){} //pareil mais dans l'autre sens
 			if (super.getPosY()-dy==i && checkDeplace(p,dx,dy)) //vérifie si la différence est la même sur l'axe Y
 				return true;
 		}
@@ -36,7 +36,7 @@ public class Dame extends Piece{
 	}
 
 	public int[][] getAllDeplacement(){
-		int[8][8] grille;
+		int grille=new int[8][8];
 		for(int i=0; i<8; i++){
 			for (int j=0; j<8; j++){
 				if (i!=super.getPosX() && j!=super.getPosY())
