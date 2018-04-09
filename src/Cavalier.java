340c6712586ad76bcb0f00c6085223ed24e1fee2
alexis <alexiscrinquand@gmail.com>
2018-04-09 18:19:53 +0200

    maj2 cavalier

--- src/Cavalier.java
+++ src/Cavalier.java
@@ -24,7 +24,7 @@ public class Cavalier extends Piece{
 	}
 
 	public int[][] getAllDeplacement(){
-		int grille=new int[8][8];
+		int grille[][] =new int[8][8];
 		for(int i=0; i<8; i++){
 			for (int j=0; j<8; j++){
 				if (((super.getPosX()+2==i)&&(super.getPosY()+1==j))||
