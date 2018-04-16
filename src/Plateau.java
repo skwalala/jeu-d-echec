2bf9e1c62e6e19e58f876ac50afba94d1377ee0f
paul jacobs <jacobspaul@yahoo.com>
2018-04-16 17:05:50 +0200

    Update Plateau.java
    changement de la disposition des pièces:
    lettres = colonnes
    chiffres = lignes
    case en haut à gauche : A1
    blanc en haut
    noir en bas

--- src/Plateau.java
+++ src/Plateau.java
@@ -8,35 +8,35 @@ class Plateau{
     for (int i = 0 ; i < 8 ; i++ ) {
       if (i == 1 ) {
         for ( int j = 0 ; j < 8 ; j++ ) {
-          this.plateau[i][j] = new Piece(i,j,"noir","P");
+          this.plateau[j][i] = new Piece(j,i,"noir","P");
         }
       }
       if (i == 6 ) {
         for ( int j = 0 ; j < 8 ; j++ ) {
-          this.plateau[i][j] = new Piece(i,j,"noir","P");
+          this.plateau[j][i] = new Piece(j,i,"noir","P");
         }
       }
       if (i != 1 && i != 6) {
-        this.plateau[0][0] = new Piece(0,0,"noir","T");
-        this.plateau[0][1] = new Piece(0,1,"noir","C");
-        this.plateau[0][2] = new Piece(0,2,"noir","F");
-        this.plateau[0][3] = new Piece(0,3,"noir","D");
-        this.plateau[0][4] = new Piece(0,4,"noir","R");
-        this.plateau[0][5] = new Piece(0,5,"noir","F");
-        this.plateau[0][6] = new Piece(0,6,"noir","C");
-        this.plateau[0][7] = new Piece(0,7,"noir","T");
+        this.plateau[0][0] = new Piece(0,0,"blanc","T");
+        this.plateau[1][0] = new Piece(1,0,"blanc","C");
+        this.plateau[2][0] = new Piece(2,0,"blanc","F");
+        this.plateau[3][0] = new Piece(3,0,"blanc","D");
+        this.plateau[4][0] = new Piece(4,0,"blanc","R");
+        this.plateau[5][0] = new Piece(5,0,"blanc","F");
+        this.plateau[6][0] = new Piece(6,0,"blanc","C");
         this.plateau[7][0] = new Piece(7,0,"blanc","T");
-        this.plateau[7][1] = new Piece(7,1,"blanc","C");
-        this.plateau[7][2] = new Piece(7,2,"blanc","F");
-        this.plateau[7][3] = new Piece(7,3,"blanc","D");
-        this.plateau[7][4] = new Piece(7,4,"blanc","R");
-        this.plateau[7][5] = new Piece(7,5,"blanc","F");
-        this.plateau[7][6] = new Piece(7,6,"blanc","C");
-        this.plateau[7][7] = new Piece(7,7,"blanc","T");
+        this.plateau[0][7] = new Piece(0,7,"noir","T");
+        this.plateau[1][7] = new Piece(1,7,"noir","C");
+        this.plateau[2][7] = new Piece(2,7,"noir","F");
+        this.plateau[3][7] = new Piece(3,7,"noir","D");
+        this.plateau[4][7] = new Piece(4,7,"noir","R");
+        this.plateau[5][7] = new Piece(5,7,"noir","F");
+        this.plateau[6][7] = new Piece(6,7,"noir","C");
+        this.plateau[7][7] = new Piece(7,7,"noir","T");
       }
       if (i > 2 && i < 6) {
         for ( int j = 0 ; j < 8 ; j++ ) {
-          this.plateau[i][j] = null;
+          this.plateau[j][i] = null;
         }
       }
     }
@@ -48,8 +48,8 @@ class Plateau{
 
       System.out.print("| ");
       for (int j = 0 ; j < 8 ; j++ ) {
-        if ((this.plateau[i][j]) instanceof Piece) {
-          System.out.print((this.plateau[i][j]).getNom() + " | ");
+        if ((this.plateau[j][i]) instanceof Piece) {
+          System.out.print((this.plateau[j][i]).getNom() + " | ");
         }else {
           System.out.print("- | ");
         }
