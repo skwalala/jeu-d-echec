bc4cf7f86d29bc1281e719ebcef36e3818925329
Daysig <kerbit.david@gmail.com>
2018-04-16 17:14:19 +0200

    Maj 2.1 (instable)
    Problème pointeur ligne 33

--- src/Jeu.java
+++ src/Jeu.java
@@ -1,18 +1,18 @@
 import java.util.*;
-public class Jeu{
-	static final Scanner input = new Scanner(System.in);
+public class Jeu {
+    static final Scanner input = new Scanner(System.in);
 
-	public static String getCoordonnéePiece(String camp){
-		System.out.println(camp.concat(" c'est à vous, qu'elle pièce vous voulez déplacé ? "));
-		return input.nextLine();
-	}
-
-	public static String getCoordonnéeDeplacement(){
-	    System.out.println("Où voulez vous déplacer votre pièce ?");
-	    return input.nextLine();
+    public static String getCoordonnéePiece(String camp) {
+        System.out.println(camp.concat(" c'est à vous, qu'elle pièce vous voulez déplacé ? "));
+        return input.nextLine();
     }
 
-	public static void verif(char[] possA, char[] possB, String camp, Piece[][] p){
+    public static String getCoordonnéeDeplacement() {
+        System.out.println("Où voulez vous déplacer votre pièce ?");
+        return input.nextLine();
+    }
+
+    public static void verif(char[] possA, char[] possB, String camp, Piece[][] p) {
         boolean pieceCorrect = false;
         boolean deplacementCorrect = false;
         int verification = 0;
@@ -20,49 +20,53 @@ public class Jeu{
         char caractYPiece;
         int xPiece = 0;
         int yPiece = 0;
-        while(!pieceCorrect){
+        while (!pieceCorrect) {
             String pieceJoue = getCoordonnéePiece(camp);
             caractXPiece = pieceJoue.charAt(0);
             caractYPiece = pieceJoue.charAt(1);
-            for (int i = 0; i<possA.length; i++) {
+            for (int i = 0; i < possA.length; i++) {
                 for (int j = 0; j < possB.length; j++) {
                     if ((pieceJoue.length() == 2) && (caractXPiece == possA[i]) && (caractYPiece == possB[j])) {
-                        if (possA[i] == caractXPiece){
-                            xPiece = i;
-                            yPiece = Character.getNumericValue(caractYPiece);
-                            verification = 1;
+                        xPiece = i;
+                        yPiece = j-1;
+                        System.out.println("xPiece = "+xPiece+" yPiece = "+yPiece);
+                        if (p[xPiece][yPiece].getCouleur() != null){
+                            if (p[xPiece][yPiece].getCouleur().equals(camp)) {
+                                verification = 1;
+                            }
                         }
                     }
                 }
             }
-            if (verification == 1){
+            if (verification == 1) {
                 pieceCorrect = true;
             } else {
-                System.out.println("Erreur : veuillez entrer une pièce se situant entre A1 et H8.");
+                System.out.println("Erreur : veuillez entrer une pièce se situant entre A1 et H8 et de votre camp.");
             }
         }
-        while(!deplacementCorrect){
-         String deplacementJoue = getCoordonnéeDeplacement();
-         char caractNewXPiece = deplacementJoue.charAt(0);
-         char caractNewYPiece = deplacementJoue.charAt(1);
-         int newXPiece = 0;
-         int newYPiece = 0;
-         System.out.println(xPiece+" "+yPiece);
-	     System.out.println(caractNewXPiece+" "+caractNewYPiece);
-	     for (int i = 0; i<possA.length; i++) {
-             if (possA[i] == caractNewXPiece){
-                 newXPiece = i;
-                 newYPiece = Character.getNumericValue(caractNewYPiece);
-             }
-         }
-         if(!p[xPiece][yPiece].seDeplace(p, newXPiece, newYPiece)){
-             System.out.println("Erreur : veuillez choisir un autre emplacement.");
-         } else {
-             p[xPiece][yPiece].seDeplace(p, newXPiece, newYPiece);
-             deplacementCorrect = true;
-         }
+        while (!deplacementCorrect) {
+            String deplacementJoue = getCoordonnéeDeplacement();
+            char caractNewXPiece = deplacementJoue.charAt(0);
+            char caractNewYPiece = deplacementJoue.charAt(1);
+            int newXPiece = 0;
+            int newYPiece = 0;
+            System.out.println(xPiece + " " + yPiece);
+            System.out.println(caractNewXPiece + " " + caractNewYPiece);
+            for (int i = 0; i < possA.length; i++) {
+                if (possA[i] == caractNewXPiece) {
+                    newXPiece = i;
+                    newYPiece = Character.getNumericValue(caractNewYPiece);
+                }
+            }
+            if (!p[xPiece][yPiece].seDeplace(p, newXPiece, newYPiece)) {
+                System.out.println("Erreur : veuillez choisir un autre emplacement.");
+            } else {
+                p[xPiece][yPiece].seDeplace(p, newXPiece, newYPiece);
+                deplacementCorrect = true;
+            }
         }
     }
+
 
     public void checkEchec(){
 	    
@@ -73,12 +77,12 @@ public class Jeu{
     	String camp = "";
     	boolean partieFinie = false;
     	char[] possA = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
-    	char[] possB = {'1', '2', '3', '4', '5', '6', '7', '8'};
+    	char[] possB = {'0','1', '2', '3', '4', '5', '6', '7'};
     	while (!partieFinie) {
     		p.affichePlateau();
-			camp = "Blanc";
+			camp = "blanc";
     		verif(possA, possB, camp,p.getPlateau());
-			camp = "Noir";
+			camp = "noir";
 		verif(possA, possB, camp,p.getPlateau());
     	}
     }
