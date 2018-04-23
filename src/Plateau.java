fbddd64c38626ee9d056c814a7169a7c0d610083
benjamin <emailpourfb90@gmail.com>
2018-04-23 14:03:17 +0200

    cor plateau

--- src/Plateau.java
+++ src/Plateau.java
@@ -1,13 +1,41 @@
 import java.util.*;
+import java.awt.*;
+import javax.swing.*;
 
-class Plateau{
+class Plateau extends JFrame{
   private Piece plateau[][];
 
   public Plateau(){
+    initPLateau();
+    this.setSize(1000,1000);
+    setVisible(true);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+  public void initPLateau(){
+    JPanel pGrille = new JPanel(new GridLayout(8,8));
+    JButton but;
+
     this.plateau = new Piece[8][8];
+    this.plateau[0][0] = new Tour(0,0,"blanc");
+    this.plateau[1][0] = new Cavalier(1,0,"blanc");
+    this.plateau[2][0] = new Fou(2,0,"blanc");
+    this.plateau[3][0] = new Dame(3,0,"blanc");
+    this.plateau[4][0] = new Roi(4,0,"blanc");
+    this.plateau[5][0] = new Fou(5,0,"blanc");
+    this.plateau[6][0] = new Cavalier(6,0,"blanc");
+    this.plateau[7][0] = new Tour(7,0,"blanc");
+    this.plateau[0][7] = new Tour(0,7,"noir");
+    this.plateau[1][7] = new Cavalier(1,7,"noir");
+    this.plateau[2][7] = new Fou(2,7,"noir");
+    this.plateau[3][7] = new Dame(3,7,"noir");
+    this.plateau[4][7] = new Roi(4,7,"noir");
+    this.plateau[5][7] = new Fou(5,7,"noir");
+    this.plateau[6][7] = new Cavalier(6,7,"noir");
+    this.plateau[7][7] = new Tour(7,7,"noir");
     for (int i = 0 ; i < 8 ; i++ ) {
       if (i == 1 ) {
-        for ( int j = 8 ; j < 8 ; j++ ) {
+        for ( int j = 0 ; j < 8 ; j++ ) {
           this.plateau[j][i] = new Pion(j,i,"blanc");
         }
       }
@@ -16,38 +44,21 @@ class Plateau{
           this.plateau[j][i] = new Pion(j,i,"noir");
         }
       }
-      if (i != 1 && i != 6) {
-        this.plateau[0][0] = new Tour(0,0,"blanc");
-        this.plateau[1][0] = new Cavalier(1,0,"blanc");
-        this.plateau[2][0] = new Fou(2,0,"blanc");
-        this.plateau[3][0] = new Dame(3,0,"blanc");
-        this.plateau[4][0] = new Roi(4,0,"blanc");
-        this.plateau[5][0] = new Fou(5,0,"blanc");
-        this.plateau[6][0] = new Cavalier(6,0,"blanc");
-        this.plateau[7][0] = new Tour(7,0,"blanc");
-        this.plateau[0][7] = new Tour(0,7,"noir");
-        this.plateau[1][7] = new Cavalier(1,7,"noir");
-        this.plateau[2][7] = new Fou(2,7,"noir");
-        this.plateau[3][7] = new Dame(3,7,"noir");
-        this.plateau[4][7] = new Roi(4,7,"noir");
-        this.plateau[5][7] = new Fou(5,7,"noir");
-        this.plateau[6][7] = new Cavalier(6,7,"noir");
-        this.plateau[7][7] = new Tour(7,7,"noir");
-      }
       if (i > 2 && i < 6) {
         for ( int j = 0 ; j < 8 ; j++ ) {
           this.plateau[j][i] = null;
         }
       }
     }
+
+  
   }
 
   public void affichePlateau(){
-    System.out.println("   A   B   C   D   E   F   G   H");
-    System.out.println(" +---+---+---+---+---+---+---+---+");
+    System.out.println("    A   B   C   D   E   F   G   H");
+    System.out.println("  +---+---+---+---+---+---+---+---+");
     for (int i = 0 ; i < 8 ; i++ ) {
-
-      System.out.print((i+1)+"| ");
+      System.out.print((i+1) + " | ");
       for (int j = 0 ; j < 8 ; j++ ) {
         if ((this.plateau[j][i]) instanceof Piece) {
           System.out.print((this.plateau[j][i]).getNom() + " | ");
@@ -68,7 +79,7 @@ class Plateau{
   * @param couleur couleur à vérifier (string) (mettre this.couleur lors de l'appel de la fonction depuis une classe pièce)
   * @return true si pièce de la même couleur à l'emplacement. sinon return false
   */
-  
+
   public boolean estOcuppeParPieceEquipe(int x, int y, String couleur){
 	if (this.plateau[x][y]!=null){
 		if (this.plateau[x][y].getCouleur().equals(couleur)){
